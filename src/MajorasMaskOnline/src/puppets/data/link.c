7d8cfba8860efbe82b0a148d21ab41acd0a67780
Psi-Hate <Psi-Hate@users.noreply.github.com>
2020-02-14 05:48:56 -0600

    Puppet now has human y pos

--- src/MajorasMaskOnline/src/puppets/data/link.c
+++ src/MajorasMaskOnline/src/puppets/data/link.c
@@ -138,7 +138,7 @@ static int MMAnimate(z64_global_t* global, int limb_number, uint32_t* display_li
 	{
 		z64_rot_t* frame_translation = (z64_rot_t*)en->current_frame_data;
 		translation->x += frame_translation->x;
-		translation->y += en->puppetData.age == MM_FORM_HUMAN ? (frame_translation->y * 0.66f) : frame_translation->y;
+		translation->y += frame_translation->y * 0.55f;
 		translation->z += frame_translation->z;
 	}
 
