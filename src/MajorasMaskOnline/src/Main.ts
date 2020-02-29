e74430dc471b89a85db67eb1736799e9556d1995
spiceywolf <SpiceyWolf.ArchaicSoft@outlook.com>
2020-02-29 01:00:59 -0500

    Fix for modloader api change

--- src/MajorasMaskOnline/src/Main.ts
+++ src/MajorasMaskOnline/src/Main.ts
@@ -2909,19 +2909,20 @@ interface ovl_meta {
     init: string;
 }
 
+import IMemory from 'modloader64_api/IMemory';
 export class OverlayPayload extends PayloadType {
     constructor(ext: string) {
         super(ext);
     }
 
-    parse(file: string, buf: Buffer, dest: Buffer) {
+    parse(file: string, buf: Buffer, dest: IMemory) {
         //console.log('Trying to allocate actor...');
         let overlay_start: number = 0x1AEFD0;
         let size = 0x02b1;
         let empty_slots: number[] = new Array<number>();
         for (let i = 0; i < size; i++) {
             let entry_start: number = overlay_start + i * 0x20;
-            let _i: number = dest.readUInt32BE(entry_start + 0x14);
+            let _i: number = dest.rdramRead32(entry_start + 0x14);
             let total = 0;
             total += _i;
             if (total === 0) {
@@ -2949,9 +2950,9 @@ export class OverlayPayload extends PayloadType {
         let addr: number = parseInt(meta.addr) + offset;
         let slot: number = empty_slots.shift() as number;
         //console.log('Assigning ' + path.parse(file).base + ' to slot ' + slot + '.');
-        dest.writeUInt32BE(0x80000000 + addr, slot * 0x20 + overlay_start + 0x14);
+        dest.rdramWrite32(slot * 0x20 + overlay_start + 0x14, 0x80000000 + addr);
         buf.writeUInt8(slot, offset + 0x1);
-        buf.copy(dest, parseInt(meta.addr));
+        dest.rdramWriteBuffer(parseInt(meta.addr), buf);
         return slot;
     }
 
